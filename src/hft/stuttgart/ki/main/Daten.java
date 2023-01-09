e119b1f970965dcfabd91a86ace1368a1f910c9b
sksek <sksek@10.92.8.146>
2023-01-09 10:22:10 +0100

    Daten eingefügt

--- src/hft/stuttgart/ki/main/Daten.java
+++ src/hft/stuttgart/ki/main/Daten.java
@@ -1,5 +1,39 @@
 package hft.stuttgart.ki.main;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+record werte(Double x1, Double x2, Double y) {
+};
+
 public class Daten {
+
+	public ArrayList<werte> datenliste;
+
+	public Daten() throws IOException {
+		datenliste = new ArrayList<>();
+		readFile();
+	}
+
+	public void readFile() throws IOException {
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("Wetter.txt"));
+			String zeile;
+			while ((zeile = reader.readLine()) != null) {
+				datenliste.add(new werte(Double.valueOf(zeile.split("\t")[0]), Double.valueOf(zeile.split("\t")[1]),
+						Double.valueOf(zeile.split("\t")[2])));
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Couldnt read the file");
+		}
+	}
+
+	public ArrayList<werte> getDatenliste() {
+		return datenliste;
+	}
 
 }
